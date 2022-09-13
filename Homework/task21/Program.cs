ed07dec274dccb49717c5e92565612214f6c5f3a
SQsMe <goldswag1996@mail.ru>
2022-09-13 03:05:37 +0300

    123

--- Homework/task21/Program.cs
+++ Homework/task21/Program.cs
@@ -1,26 +1,11 @@
-﻿/* Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
-A (3,6,8); B (2,1,-7), -> 15.84
-A (7,-5, 0); B (1,-1,9) -> 11.53
+﻿/* Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125
 */
 
-System.Console.Write("Координатy X первой точки:");
-int x1 = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите число: ");
+double n = Convert.ToInt32(Console.ReadLine());
 
-System.Console.Write("Координатy Y первой точки:");
-int y1 = Convert.ToInt32(Console.ReadLine());
-
-System.Console.Write("Координатy Z первой точки:");
-int z1 = Convert.ToInt32(Console.ReadLine());
-
-System.Console.Write("Координатy X второй точки:");
-int x2 = Convert.ToInt32(Console.ReadLine());
-
-System.Console.Write("Координатy Y второй точки:");
-int y2 = Convert.ToInt32(Console.ReadLine());
-
-System.Console.Write("Координатy Z второй точки:");
-int z2 = Convert.ToInt32(Console.ReadLine());
-
-         
-double result = Math.Sqrt(Math.Pow ((x2-x1),2) + Math.Pow((y2-y1),2) + Math.Pow((z2-z1),2));
-System.Console.WriteLine(result);
+for (int a = 1; a <= n; a++){ /* for = аналог while */
+System.Console.WriteLine(Math.Pow(a,3));
+} 
